c958ac0cfeaa918c2241b5cd9c6d3883126ba9aa
Candra Purnama <87640957+chandchand@users.noreply.github.com>
2021-07-22 09:05:36 +0700

    Membuat Class Controller
    OngoingProject Spring Boot & Angular

---
+++ candidatapp/src/main/java/com/belajarspring/candidatapp/controller/CandidateController.java
@@ -0,0 +1,37 @@
+package com.belajarspring.candidatapp.controller;
+
+import com.belajarspring.candidatapp.dao.CandidateDao;
+import com.belajarspring.candidatapp.dto.SearchData;
+import com.belajarspring.candidatapp.entity.Candidate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/candidate")
+public class CandidateController {
+
+    @Autowired
+    private CandidateDao candidateDao;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Candidate insert(@RequestBody Candidate candidate){
+        return candidateDao.insert(candidate);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    List<Candidate> findAll(){
+        return candidateDao.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public boolean delete(@PathVariable Long id){
+        return candidateDao.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/search")
+    public List<Candidate> search(@RequestBody SearchData searchData){
+        return candidateDao.findByName(searchData.getSearchKey());
+    }
+}
